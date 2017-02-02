636c9fe42023a4d5beef757748b18c3439222b12
Maria Belgrader <mbelgrader5@gmail.com>
2017-02-01 16:55:14 -0800

    Add ring buffer

--- lib/ring_buffer.rb
+++ lib/ring_buffer.rb
@@ -5,32 +5,52 @@ class RingBuffer
 
   def initialize
     @store = StaticArray.new(8)
-    @capacity = 8
-
+    @capacity, @start_idx, @length = 8, 0, 0
   end
 
-  # O(1)
   def [](index)
+    check_index(index)
+    store[(start_idx + index) % capacity]
   end
 
-  # O(1)
   def []=(index, val)
+    check_index(index)
+    store[(start_idx + index) % capacity] = val
   end
 
-  # O(1)
   def pop
+    raise 'index out of bounds' unless length > 0
+
+    val, self[length - 1] = self[length - 1], nil
+    @length -= 1
+
+    val
   end
 
-  # O(1) ammortized
   def push(val)
+    resize! if length == capacity
+    @length += 1
+    self[length - 1] = val
+
+    nil
   end
 
-  # O(1)
   def shift
+    raise "index out of bounds" if (length == 0)
+
+    val, self[0] = self[0], nil
+    @start_idx = (start_idx + 1) % capacity
+    @length -= 1
+
+    val
   end
 
-  # O(1) ammortized
   def unshift(val)
+    resize! if (length == capacity)
+
+    @start_idx = (start_idx - 1) % capacity
+    @length += 1
+    self[0] = val
   end
 
   protected
@@ -38,8 +58,18 @@ class RingBuffer
   attr_writer :length
 
   def check_index(index)
+    unless (index >= 0) && (index < length)
+      raise "index out of bounds"
+    end
   end
 
   def resize!
+    new_capacity = capacity * 2
+    new_store = StaticArray.new(new_capacity)
+    length.times { |i| new_store[i] = self[i] }
+
+    self.capacity = new_capacity
+    self.store = new_store
+    self.start_idx = 0
   end
 end
